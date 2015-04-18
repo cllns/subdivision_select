85f232b04d758ebba00784794c976768d96cac1a
Sean Collins <sean@cllns.com>
2015-04-18 19:47:23 -0400

    Refactor javascript into a more idiomatic, robust solution (I hope...)

--- app/assets/javascripts/subdivision_select.js
+++ app/assets/javascripts/subdivision_select.js
@@ -1,43 +1,65 @@
-$(document).ready(function() {
-  function updateSubdivisionsSelect(newSubdivisions) {
-    var subdivisionSelect = $("select[id$=_subdivision]");
+var SubdivisionSelect = (function() {
+  function SubdivisionSelect(element) {
+    this._element = $(element);
+  };
+
+  SubdivisionSelect.subdivisionSelector = "select[id$=subdivision]";
+  SubdivisionSelect.countrySelector = "select[id$=country]";
+
+  SubdivisionSelect.init = function () {
+    var klass = this;
+
+    return $(klass.subdivisionSelector).each(function() {
+      return new klass(this).init();
+    });
+  };
+
+  SubdivisionSelect.prototype.init = function() {
+    var self = this;
+
+    $(this.constructor.countrySelector).change(function() {
+      $.ajax( {
+        url: "/subdivisions",
+        data: { country_code: $(this).val() }
+      }).done(function(newSubdivisions) {
+        self._clearSubdivisionSelect();
+        self._updateSubdivisionSelect(newSubdivisions);
+      });
+    });
+  };
+
+  SubdivisionSelect.prototype._updateSubdivisionSelect = function(newSubdivisions) {
+    var self = this;
     var isEmpty = $.isEmptyObject(newSubdivisions);
 
-    clearSelect(subdivisionSelect);
-
     $.each(newSubdivisions, function(alpha2, name) {
-      subdivisionSelect.append($("<option></option>").attr("value", alpha2).text(name));
+      self._element.append($("<option></option>").attr("value", alpha2).text(name));
     });
 
     // Disable the select if there are no newSubdivisions (and un-do that once there are some)
-    subdivisionSelect.prop("disabled", isEmpty);
+    self._element.prop("disabled", isEmpty);
 
     // If there are none, make it say "none"
     if (isEmpty) {
       subdivisionSelect.append($("<option></option>").text("none"));
     }
-  }
+  };
 
   // Not only empty the select, but:
   // if the first element is blank, add a blank element before all others
-  function clearSelect(selectToClear) {
-    var includeBlank = selectToClear.children().first().text() === "";
+  SubdivisionSelect.prototype._clearSubdivisionSelect = function() {
+    var includeBlank = this._element.children().first().text() === "";
 
-    selectToClear.empty();
+    this._element.empty();
 
     if (includeBlank) {
-      selectToClear.append($("<option></option>"));
+      this._element.append($("<option></option>"));
     }
-  }
+  };
 
+  return SubdivisionSelect;
+})();
 
-  function populateSelectViaAjax() {
-    $.ajax( {
-      url: "/subdivisions",
-      data: { country_code: $(this).val() },
-      success: updateSubdivisionsSelect
-    } );
-  }
-
-  $("select[id$=_country]").change(populateSelectViaAjax);
+$(function() {
+  SubdivisionSelect.init();
 });
