be842495604021f2c491e45fa7492ffddbba83fd
Sean Collins <sean@cllns.com>
2015-04-30 12:05:36 -0400

    Require country_select

--- lib/subdivision_select/engine.rb
+++ lib/subdivision_select/engine.rb
@@ -1,11 +1,17 @@
-require "subdivision_select/subdivision_select_helper";
+require "subdivision_select/subdivision_select_helper"
 
 module SubdivisionSelect
   class Engine < ::Rails::Engine
     isolate_namespace SubdivisionSelect
 
     require "jquery-rails"
+    # "country_select" requires "countries", but it's separated here because:
+    #   - `countries` is used directly
+    #   - `country_select` is so users don't have to add country_select to their
+    #                      Gemfile, since it's required
+    # (so we could have just included the latter, but this is more clear)
     require "countries"
+    require "country_select"
 
     config.generators do |g|
       g.test_framework :rspec
